2b819e16248ffe342feb3d34cf9ceedaa12ccfe7
ystskm <brilliantpenguin@gmail.com>
2013-11-01 13:38:20 +0900

    test ok

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // node-localelist index.js
 var path = require('path'), fs = require('fs');
 
-var GLOBAL_LANG = '_global', Lbox = {}, Sbox = {};
+var GLOBAL_LANG = '_global', LISTS_DIR = path.join(__dirname, 'lists');
+var Lbox = {}, Sbox = {};
 var Locales = module.exports = function() {
 };
 
@@ -41,21 +42,22 @@ function data(lo, ln, val) {
   return Lbox[a[1]][ln];
 }
 
-fs.readdirSync(__dirname).forEach(function(fnam) {
+fs.readdirSync(LISTS_DIR).forEach(function(fnam) {
   var fnam_a = fnam.split('.');
   // ignore unexpected file_name pattern
   if(fnam_a.length != 2 || fnam_a[0].length != 2 && fnam_a[0] != GLOBAL_LANG)
     return;
-  fs.readFileSync(path.join(__dirname, fnam)).split("\n").forEach(function(l) {
+  var lnam = fnam_a[0], buf = fs.readFileSync(path.join(LISTS_DIR, fnam));
+  buf.toString().split(/\n/).forEach(function(l) {
     // split line with "|" 
     // https://tools.cloudplus.me/UTF8Stringifier/
     var d = l.split("\u007c");
-    if('string' != typeof l[0])
+    if('string' != typeof d[0])
       return;
     // set to language box
-    var l0 = l[0], ls = shorten(l0);
-    !Sbox[ls] && (Sbox[ls] = l[0]);
+    var l0 = d[0], ls = shorten(l0);
+    !Sbox[ls] && (Sbox[ls] = d[0]);
     !Lbox[l0] && (Lbox[l0] = {});
-    Lbox[l.shift()][fnam] = l.join(' ');
+    Lbox[d.shift()][lnam] = d.join(' ');
   });
 });
