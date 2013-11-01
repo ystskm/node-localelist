e0ebc19cbba0b1b0f65928c0788d3ac356ebc9cc
ystskm <brilliantpenguin@gmail.com>
2013-11-01 16:16:25 +0900

    files for test

--- Makefile
+++
@@ -1,3 +0,0 @@
-test:
-	@nodeunit test
-.PHONY: test
--- test/basic.js
+++ test/basic.js
@@ -1,27 +1,27 @@
-var assert = require('assert');
+var nodeunit = require('nodeunit');
 var ll = require('../index');
 
 module.exports = nodeunit.testCase({
-  'shorten': function() {
-    assert.equal(typeof ll.shorten, 'function');
-    assert.equal(ll.shorten('ja_JP'), '!ja');
-    assert.done();
+  'shorten': function(t) {
+    t.equal(typeof ll.shorten, 'function');
+    t.equal(ll.shorten('ja_JP'), 'ja');
+    t.done();
   },
-  'toArray': function() {
-    assert.equal(typeof ll.toArray, 'function');
+  'toArray': function(t) {
+    t.equal(typeof ll.toArray, 'function');
     var res = ll.toArray('ja_JP');
-    assert.ok(Array.isArray(res));
-    assert.equal(res[0] == 'ja'), t.equal(res[1] == 'ja_JP');
-    assert.done();
+    t.ok(Array.isArray(res));
+    t.equal(res[0] == 'ja'), t.equal(res[1] == 'ja_JP');
+    t.done();
   },
-  'data': function() {
-    assert.equal(typeof ll.data, 'function');
-    assert.equal(ll.data('ja_JP', 'ja'), '日本語 日本');
-    assert.done();
+  'data': function(t) {
+    t.equal(typeof ll.data, 'function');
+    t.equal(ll.data('ja_JP', 'ja'), '日本語 日本');
+    t.done();
   },
-  'list': function() {
-    assert.equal(typeof ll.list, 'function');
-    assert.ok(Array.isArray(ll.list()));
-    assert.done();
+  'list': function(t) {
+    t.equal(typeof ll.list, 'function');
+    t.ok(Array.isArray(ll.list()));
+    t.done();
   }
 });
